22629426d828548ca22b0904634028e24e061a94
barma <barma@gmail.com>
2024-04-30 23:49:28 +0530

    added devops

---
+++ redhat/software management/command.py
@@ -0,0 +1,18 @@
+'''Commonly used YUM commands:
+sudo >here you can replace yum with dnf'''
+
+# sudo yum check-update
+# sudo yum update
+# yum list
+# yum install package_name: Installs the specified packages.
+# yum remove package_name: Removes the specified packages.
+# yum search keyword: Searches package metadata for keywords.
+# yum info package_name: Lists package information.
+# yum update: Updates all packages to the latest version.
+# yum repolist: Lists repositories.
+# yum history: Displays past transactions.
+
+# curl -O https:// software url.tar
+# tar xzf apache-maven-3.9.6-bin.tar.gz
+# sudo mv apache-maven-3.9.6 /opt/maven
+# cd /opt/maven/bin>./mvn --version
