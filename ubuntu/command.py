8886d5f24c664c7c255a041eeeea6efaddc231b8
barma <barma@gmail.com>
2024-05-12 07:33:36 +0530

    added command

---
+++ ubuntu/command.py
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt install openjdk-17-jdk -y
+
+cd /tmp
+wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+tar -xvf apache-maven-3.9.6-bin.tar.gz
+sudo mv apache-maven-3.9.6 /opt/maven
+
+/opt/maven/bin
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
